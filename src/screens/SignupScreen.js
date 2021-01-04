0cce1471237845543c73672af0f58a0e60d86477
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2021-01-04 17:36:17 +0100

    lagt til screens

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 40 }}>SignupScreen</Text>;
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 40 }}>SignupScreen</Text>;
+      <Button
+        title="Go to Signin"
+        onPress={() => navigation.navigate("Signin")}
+      />
+      <Button
+        title="Go to main flow"
+        onPress={() => navigation.navigate("mainFlow")}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const TrackListScreen = () => {
-  return <Text style={{ fontSize: 40 }}>TrackListScreen</Text>;
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 40 }}>TrackListScreen</Text>;
+      <Button
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate("trackDetail")}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
